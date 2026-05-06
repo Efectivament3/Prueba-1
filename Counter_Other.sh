dfb163945cd2941bcaaa32c02c858c5ee1d41be4
Efectivament3 <danieleduardo.gonzalez19@gmail.com>
2026-05-05 23:04:29 -0500

    Counter_Other.sh

--- Counter_Other.sh
+++ Counter_Other.sh
@@ -1,8 +1,84 @@
+#!/bin/bash
 #Basic  counter and its variations
+#Este progama basico, trbajaremos comando basicos, el programa sera dividio en 4 tipo de contadores, cado uno con sus caracteristicas o  "features, en cada caso particual se explicara la caractreristicas del mismo y se el dara unas opciones al usuario 
+#Una discula de ante mano por la Ortografia  :P
+ #declaramos una funcion para no copiar y pegar lo mismo inifnitas veces
+#Ademas vamos a declarar super vriables, las cuales seran las calculadoras por separado, luego mosntraremos
+#el main menu en la pantalla
 
-echo	"     Counter"
-echo	"---------------"
-echo	"Choose from 1 to 5 your kind of counter"
-echo 	"//////////////////////"
-echo	"1.Basic(from 1 to 100)" 
-echo	"2. Advance()"
+basic_counter() {
+#en este contador, el sistema contara de el 1 al 100, es el mas basico
+while true; do
+
+	    echo "You choose option 1"
+	    echo "We are going to count from 1 to 100 without any feature"
+	    echo "So lets start"
+		for number in {1..100}
+		do
+	   	 echo "Counter: $number"
+		done
+	echo "as you saw the task was done susccesfully, would you like to return to the main menu?"
+	echo "YES/NO"
+		read ex1t
+		case $ex1t in
+			YES)
+				echo  "going back to the menue"
+				break
+			;;
+			NO)
+				echo "Shutthing down the program"
+				exit
+			;;
+			*)
+
+				echo "Type a valid word"
+				sleep 1
+			;;
+		esac
+done
+}
+
+
+main_menu() {
+
+while true; do
+	echo ""
+	echo ""
+	echo ""
+	echo ""
+	echo ""
+	echo ""
+	echo	"Hello there! this a simple code with 4 programs that each counts from  1 to 100, but with some differeces from each other"
+
+	echo	"---------------"
+	echo	"        ////// COUNTER///////                By Efectivamet3 :p"
+	echo	"---------------"
+	echo	"Choose from 1 to 5 to choose your type of counter"
+	echo 	"/////////////////////////////////////"
+	echo	"      1.Basic(from 1 to 100)"
+	echo	"      2.Medium()"
+	echo	"      3.Advance()"
+	echo	"      4.Master()"
+
+	#Hacemos que el usario escoja un tipo de contador
+
+	read option  #El sistema leera, la opcion
+
+	case  $option in # se desplegara una calcualdora, o case de las 4 segun la opcion escogida
+
+	  1)basic_counter #muestra la super variable declarada con las lineas del contador basica
+		;;
+	  2)medium_counter #muestra la super variable declarada con las lineas del contador medium
+		;;
+	  3)advance_counter #muestra la super variable declarada con las lineas del contador advance
+		;;
+	  4)master_counter #muestra la super variable declarada con las lineas del contador master
+		;;
+  	  *)echo "Opción inválida"
+		;;
+	esac
+	done
+}
+
+
+main_menu #mostramos el progama en la pantalla
