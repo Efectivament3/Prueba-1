dc44cc4b26398cc76c5c82840a146106ddf25e6a
Efectivament3 <danieleduardo.gonzalez19@gmail.com>
2026-05-05 19:46:41 -0500

    Counter_Other.sh

---
+++ Counter_Other.sh
@@ -0,0 +1,8 @@
+#Basic  counter and its variations
+
+echo	"     Counter"
+echo	"---------------"
+echo	"Choose from 1 to 5 your kind of counter"
+echo 	"//////////////////////"
+echo	"1.Basic(from 1 to 100)" 
+echo	"2. Advance()"
