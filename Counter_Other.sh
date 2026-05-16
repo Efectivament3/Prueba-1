950228130f105be50662c00523b4f174cc616681
Efectivament3 <danieleduardo.gonzalez19@gmail.com>
2026-05-15 19:33:47 -0500

    Counter_Other.sh

--- Counter_Other.sh
+++ Counter_Other.sh
@@ -13,12 +13,14 @@ while true; do
 	    echo "You choose option 1"
 	    echo "We are going to count from 1 to 100 without any feature"
 	    echo "So lets start"
+sleep 3
 		for number in {1..100}
 		do
 	   	 echo "Counter: $number"
 		done
 	echo "as you saw the task was done susccesfully, would you like to return to the main menu?"
 	echo "YES/NO"
+
 		read ex1t
 		case $ex1t in
 			YES)
@@ -30,14 +32,128 @@ while true; do
 				exit
 			;;
 			*)
+				echo "Invild enter, type YES/NO"
 
-				echo "Type a valid word"
-				sleep 1
-			;;
 		esac
 done
 }
+#----------------------------------------------------------------------------------------------------------
+medium_counter() {
+# en este contador, el sistema contara de el 1 al 100, mostrando los numero pares
+while true; do
 
+	    echo "You choose option 2"
+	    echo "We are going to count from 1 to 100 only showing the pair numbers"
+	    echo "So lets start"
+sleep 3
+		paircounter=1 #desde donde empezamos a contar
+
+		while [ $paircounter -le 100 ] #para saber hasta que numero contamos
+		do
+	    		if [ $((paircounter % 2)) -eq 0 ] #cuando el residuo de la divison es 0 se muestra el numero en la pantalla
+		    	then
+        			echo $paircounter
+   			 fi
+
+    		paircounter=$((paircounter + 1))
+done
+	echo "as you saw the task was done susccesfully, would you like to return to the main menu?"
+	echo "YES/NO"
+
+		read ex1t
+		case $ex1t in
+			YES)
+				echo  "going back to the menue"
+				break
+			;;
+			NO)
+				echo "Shutthing down the program"
+				exit
+			;;
+			*)
+				echo "Invild enter"
+
+		esac
+done
+}
+advance_counter() {
+# en este contador, el sistema contara de el 1 al 100, ademas se le preguntara al usuario hasta donde quiere contar
+while true; do
+
+	    echo "You choose option 3"
+	    echo "In this counter you can choose until what number we are going to count"
+            echo "It needs to be a possitive number and no bigger than 100 diffent of cero"
+		read -p "Type a number: " optionelected
+
+	until [[ "$numero" =~ ^[0-9]+$ ]] && [ "$numero" -gt 0 ] && [ "$numero" -le 100 ]
+	do
+    echo "Invalid number"
+
+    read -p "Type Cagain: " optionelected
+done
+
+echo "Valid number"
+sleep 3
+	    echo "We are going to count from 1 to $optionelected"
+	    echo "So lets start"
+sleep 4
+		for ((number=1; number<=optionelected; number++)) #emepezamos desde el numero 1 con el number=1 y luego el numero va hasta donde el usuario indico, y luego inicia el contador, sumandole 1 con los simbolos ++
+		do
+    		echo "Counter: $number"
+		done
+	echo "as you saw the task was done susccesfully, would you like to return to the main menu?"
+	echo "YES/NO"
+
+		read ex1t
+		case $ex1t in
+			YES)
+				echo  "going back to the menue"
+				break
+			;;
+			NO)
+				echo "Shutthing down the program"
+				exit
+			;;
+			*)
+				echo "Invild enter"
+
+		esac
+
+done
+}
+basic_counter() {
+# en este contador, el sistema contara de el 1 al 100, es el mas basico
+while true; do
+
+	    echo "You choose option 1"
+	    echo "We are going to count from 1 to 100 without any feature"
+	    echo "So lets start"
+		for number in {1..100}
+		do
+	   	 echo "Counter: $number"
+		done
+	echo "as you saw the task was done susccesfully, would you like to return to the main menu?"
+	echo "YES/NO"
+
+		read ex1t
+		case $ex1t in
+			YES)
+				echo  "going back to the menue"
+				break
+			;;
+			NO)
+				echo "Shutthing down the program"
+				exit
+			;;
+			*)
+				echo "Invild enter"
+
+		esac
+
+done
+}
+
+#--------------------------------------------------------------------------------------------------------------------
 
 main_menu() {
 
@@ -56,8 +172,8 @@ while true; do
 	echo	"Choose from 1 to 5 to choose your type of counter"
 	echo 	"/////////////////////////////////////"
 	echo	"      1.Basic(from 1 to 100)"
-	echo	"      2.Medium()"
-	echo	"      3.Advance()"
+	echo	"      2.Medium(only pair numbers)"
+	echo	"      3.Advance(aks to stop in a specific value)"
 	echo	"      4.Master()"
 
 	# Hacemos que el usario escoja un tipo de contador
